ce2a01fd1b09a69dfa6e66be9d9987910fc7b909
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2015-12-31 03:36:28 +0530

    addo file

--- app/src/main/java/com/example/chathuranga_pamba/sonitcabs_passenger/CustomAutoCompleteTextView.java
+++ app/src/main/java/com/example/chathuranga_pamba/sonitcabs_passenger/CustomAutoCompleteTextView.java
@@ -3,5 +3,26 @@ package com.example.chathuranga_pamba.sonitcabs_passenger;
 /**
  * Created by Chathuranga-Pamba on 15/12/31.
  */
-public class CustomAutoCompleteTextView {
+import java.util.HashMap;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.AutoCompleteTextView;
+
+/** Customizing AutoCompleteTextView to return Place Description
+ * corresponding to the selected item
+ */
+public class CustomAutoCompleteTextView extends AutoCompleteTextView {
+
+    public CustomAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    /** Returns the place description corresponding to the selected item */
+    @Override
+    protected CharSequence convertSelectionToString(Object selectedItem) {
+        /** Each item in the autocompetetextview suggestion list is a hashmap object */
+        HashMap<String, String> hm = (HashMap<String, String>) selectedItem;
+        return hm.get("description");
+    }
 }
